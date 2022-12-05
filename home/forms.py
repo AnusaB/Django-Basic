30c38a5bace8b995efdfd689647241c4b13b15e3
Anusha <anushabhndr@gmail.com>
2022-12-05 12:08:45 +0545

    First Commit

---
+++ home/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from . models import Books,Author
+
+
+class AddBookForm(forms.ModelForm):
+    class Meta:
+        model = Books
+        fields = '__all__'
+
+class AuthorModelForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
